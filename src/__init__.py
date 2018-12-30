aa0da397d59c5df772560649b5cdf8b27c6051f7
Machine[3R] <machine-3R@lanenga.net>
2018-12-30 17:04:16 +0100

    made program runnable from editor

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,11 @@
-from .application import Application
-from .gui import Control, Result
+
+if __name__ == "__main__":
+	from application import Application
+	from gui import Control, Result
+
+	controlGUI = Control()
+	resultGUI = Result()
+	Application(controlGUI, resultGUI)
+else:
+	from .application import Application
+	from .gui import Control, Result
